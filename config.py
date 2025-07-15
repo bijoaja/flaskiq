acd6a524675fa3055405f22dbd5df81f8735d288
bijoaja <joelbinsar@gmail.com>
2025-07-15 16:02:59 +0700

    upd: change identity to flaskiq

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     # KEY UNTUK AI dan SECRET Key
     SECRET_KEY = os.environ.get('SECRET_KEY')
     AI_KEY = os.environ.get("AI_KEY")
+    OLLAMA_HOST = os.environ.get("OLLAMA_HOST")
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
